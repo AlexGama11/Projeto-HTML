018c3682d35eee0c3d338c7f41e4c81e0a686b33
Alex Gama <alexgama1612@gmail.com>
2023-01-10 16:45:48 +0000

    adding login

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,24 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+const loginDetails = {username: "admin", password: "password", nomeRestaurante: "McDonald's", nível: "20"};
+
+var unameInput = document.getElementById("uname");
+var pwordInput = document.getElementById("pword");
+
+function validateForm() {
+  let x = document.forms["loginForm"]["uname"].value;
+  let y = document.forms["loginForm"]["pword"].value;
+
+  if (x != loginDetails.username || y != loginDetails.password) {
+    alert("O campo está vazio, ou a informção está errada!");
+    return false;
+  }
+
+  else
+  {
+    window.location.replace("https://www.alexmango.tk");
+    return true;
+  }
+}
