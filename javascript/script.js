b33c1ea502669fa8f09c0eb2503a3e97fd38adba
Alex Gama <alexgama1612@gmail.com>
2023-01-10 15:10:12 +0000

    topnav added

--- javascript/script.js
+++ javascript/script.js
@@ -3,16 +3,12 @@
 
 console.log("Hello World!");
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-  document.getElementById("sidemenu").style.width = "250px";
- // document.getElementById("main").style.marginLeft = "250px";
-  //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-  document.getElementById("sidemenu").style.width = "0";
- // document.getElementById("main").style.marginLeft = "0";
-  //document.body.style.backgroundColor = "white";
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
