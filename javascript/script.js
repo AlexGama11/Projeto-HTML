3821821cc87339ce6f2115201420c2d885219b35
Alex Gama <alexgama1612@gmail.com>
2023-01-11 11:35:09 +0000

    test

--- javascript/script.js
+++ javascript/script.js
@@ -12,29 +12,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-const loginDetails = {username: "admin", password: "password", nomeRestaurante: "McDonald's", nível: "20"};
-
-var unameInput = document.getElementById("uname");
-var pwordInput = document.getElementById("pword");
-
-function validateForm() {
-  //ver php para submeter o form!!!
-  //event.preventDefault();
-  let x = document.forms["loginForm"]["uname"].value;
-  let y = document.forms["loginForm"]["pword"].value;
-
-  if (x != loginDetails.username || y != loginDetails.password) {
-    alert("O campo está vazio, ou a informção está errada!");
-    return false;
-  }
-
-  else
-  {
-    window.location.replace("https://www.alexmango.tk");
-    return true;
-  }
-
-  alert(`Username:${x},\nPassword::${y}.`);
-  console.log(`Username:${x},\nPassword::${y}.`);
-}
